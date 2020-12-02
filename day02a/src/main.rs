783f37bdcc6b04d37f192f34815c88a1c008c336
Jeff Lee <jeffomatic@gmail.com>
2020-12-01 21:22:00 -0800

    day02a: finish

---
+++ day02a/src/main.rs
@@ -0,0 +1,36 @@
+use regex::Regex;
+use std::io::{self, Read};
+
+fn main() {
+    let re = Regex::new(r"^(\d+)-(\d+) ([a-z]): ([a-z]+)$").unwrap();
+
+    let input = get_input();
+    let mut valid = 0;
+
+    for s in input.split("\n") {
+        let caps = re.captures(s).unwrap();
+        let from = caps[1].parse::<i32>().unwrap();
+        let to: i32 = caps[2].parse::<i32>().unwrap();
+        let want: char = caps[3].parse::<char>().unwrap();
+
+        let pw = &caps[4];
+        let mut got = 0;
+        for c in pw.chars() {
+            if c == want {
+                got += 1;
+            }
+        }
+
+        if from <= got && got <= to {
+            valid += 1;
+        }
+    }
+
+    println!("{}", valid);
+}
+
+fn get_input() -> String {
+    let mut input = String::new();
+    io::stdin().lock().read_to_string(&mut input).unwrap();
+    return input.trim().to_string();
+}
