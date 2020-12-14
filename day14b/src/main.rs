596f78f586e0f9032324ad5aff2539ff395c167a
Jeff Lee <jeffomatic@gmail.com>
2020-12-13 22:51:20 -0800

    day14b: cleanup

--- day14b/src/main.rs
+++ day14b/src/main.rs
@@ -4,6 +4,7 @@ use std::{
 };
 
 use regex::Regex;
+
 #[derive(Debug, Copy, Clone, PartialEq)]
 enum Bit {
     Zero,
@@ -17,31 +18,24 @@ enum Op {
     AssignMem(usize, u64),
 }
 
-fn set_bit(v: usize, bit: usize) -> usize {
-    v | 1 << bit
-}
-
-fn clear_bit(v: usize, bit: usize) -> usize {
-    v & !(1 << bit)
+fn set_bit(v: usize, bit: usize, on: bool) -> usize {
+    if on {
+        v | 1 << bit
+    } else {
+        v & !(1 << bit)
+    }
 }
 
 fn distribute_bits(mut addr: usize, bitvec: &Vec<bool>, targets: &Vec<usize>) -> usize {
     for (i, b) in bitvec.iter().enumerate() {
         let target_bit = targets[i];
-        addr = if *b {
-            set_bit(addr, target_bit)
-        } else {
-            clear_bit(addr, target_bit)
-        };
+        addr = set_bit(addr, target_bit, *b);
     }
     addr
 }
 
 fn to_bitvec(bits: u64, num_bits: usize) -> Vec<bool> {
-    (0..num_bits).fold(Vec::new(), |mut accum, b| {
-        accum.push((bits & (1 << b)) == 0);
-        accum
-    })
+    (0..num_bits).map(|n| bits & 1 << n == 0).collect()
 }
 
 fn munge_addr(addr: usize, mask: &Vec<Bit>) -> Vec<usize> {
@@ -49,53 +43,55 @@ fn munge_addr(addr: usize, mask: &Vec<Bit>) -> Vec<usize> {
     let mut base_addr = addr;
     for (i, b) in mask.iter().enumerate() {
         if *b == Bit::One {
-            base_addr = set_bit(base_addr, i);
+            base_addr = set_bit(base_addr, i, true);
         }
     }
 
-    // Generate all possible floating bits
-    let floating_bits: Vec<usize> = mask
+    // Generate the full set of values for all floating bits
+    let floating_locs: Vec<usize> = mask
         .iter()
         .enumerate()
         .filter(|(_i, b)| **b == Bit::Float)
         .map(|(i, _b)| i)
         .collect();
 
-    let mask_size = floating_bits.len();
-    let mut addrs: Vec<usize> = Vec::new();
-    for mask in 0..(1 << mask_size) {
-        let mask_vec = to_bitvec(mask, mask_size);
-        addrs.push(distribute_bits(base_addr, &mask_vec, &floating_bits));
-    }
-
-    addrs
+    let mask_size = floating_locs.len();
+    (0..(1 << mask_size))
+        .map(|mask| {
+            let mask_vec = to_bitvec(mask, mask_size);
+            distribute_bits(base_addr, &mask_vec, &floating_locs)
+        })
+        .collect()
 }
 
 fn main() {
     let mask_re = Regex::new(r#"mask = ([X01]{36})"#).unwrap();
     let assign_re = Regex::new(r#"mem\[(\d+)\] = (\d+)"#).unwrap();
 
-    let mut program: Vec<Op> = Vec::new();
-    for line in get_input().lines() {
-        if let Some(caps) = mask_re.captures(line) {
-            let mut mask = Vec::new();
-            for c in caps[1].chars().rev() {
-                mask.push(match c {
-                    '0' => Bit::Zero,
-                    '1' => Bit::One,
-                    'X' => Bit::Float,
-                    _ => unreachable!(),
-                });
+    let program: Vec<Op> = get_input()
+        .lines()
+        .map(|line| {
+            if let Some(caps) = mask_re.captures(line) {
+                let mut mask = Vec::new();
+                for c in caps[1].chars().rev() {
+                    mask.push(match c {
+                        '0' => Bit::Zero,
+                        '1' => Bit::One,
+                        'X' => Bit::Float,
+                        _ => unreachable!(),
+                    });
+                }
+
+                Op::SetMask(mask.clone())
+            } else if let Some(caps) = assign_re.captures(line) {
+                let addr: usize = caps[1].parse().unwrap();
+                let val: u64 = caps[2].parse().unwrap();
+                Op::AssignMem(addr, val)
+            } else {
+                panic!("could not parse {}", line);
             }
-            program.push(Op::SetMask(mask.clone()));
-        } else if let Some(caps) = assign_re.captures(line) {
-            let addr: usize = caps[1].parse().unwrap();
-            let val: u64 = caps[2].parse().unwrap();
-            program.push(Op::AssignMem(addr, val));
-        } else {
-            panic!("could not parse {}", line);
-        }
-    }
+        })
+        .collect();
 
     let mut mem: HashMap<usize, u64> = HashMap::new();
     let mut current_mask = Vec::new();
@@ -110,7 +106,7 @@ fn main() {
         }
     }
 
-    let res = mem.iter().fold(0, |accum, (_, v)| accum + v);
+    let res = mem.values().fold(0, |accum, v| accum + v);
     println!("{}", res);
 }
 
