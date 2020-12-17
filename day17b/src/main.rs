3535a968d376b40a189998aa1b736cd7ab80f5ea
Jeff Lee <jeffomatic@gmail.com>
2020-12-17 09:55:04 -0800

    day17x: more cleanup

--- day17b/src/main.rs
+++ day17b/src/main.rs
@@ -17,39 +17,37 @@ fn main() {
 
     for _ in 0..6 {
         let mut next_active: HashSet<Pos> = HashSet::new();
-        let mut neighbors_for_inactives: HashMap<Pos, usize> = HashMap::new();
-
+        let mut neighbor_count: HashMap<Pos, usize> = HashMap::new();
         for (x, y, z, w) in active.iter() {
-            let current = (*x, *y, *z, *w);
-            let mut active_neighbors = 0;
             for dx in -1..=1 {
                 for dy in -1..=1 {
                     for dz in -1..=1 {
                         for dw in -1..=1 {
-                            if dx == 0 && dy == 0 && dz == 0 && dw == 0 {
-                                continue;
-                            }
-
-                            let other = (x + dx, y + dy, z + dz, w + dw);
-
-                            if active.contains(&other) {
-                                active_neighbors += 1;
-                            } else {
-                                *neighbors_for_inactives.entry(other).or_insert(0) += 1;
+                            if dx != 0 || dy != 0 || dz != 0 || dw != 0 {
+                                let other = (x + dx, y + dy, z + dz, w + dw);
+                                *neighbor_count.entry(other).or_insert(0) += 1;
                             }
                         }
                     }
                 }
             }
+        }
 
-            // Survival check
-            if active_neighbors == 2 || active_neighbors == 3 {
-                next_active.insert(current);
+        // Surival check
+        for p in active.iter() {
+            match neighbor_count.get(p) {
+                Some(2) | Some(3) => {
+                    next_active.insert(*p);
+                }
+                _ => (),
             }
         }
 
         // Spawn check
-        for (pos, active_neighbors) in neighbors_for_inactives.iter() {
+        for (pos, active_neighbors) in neighbor_count.iter() {
+            if active.contains(pos) {
+                continue;
+            }
             if *active_neighbors == 3 {
                 next_active.insert(*pos);
             }
